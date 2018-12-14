1a5977842f3c35fe6ffabe04dc357e2a68dccb09
USER <user@USER-no-MacBook-Air-2.local>
2018-12-14 18:48:04 +0900

    blog,user,favoriteのコードの実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,73 @@
 class BlogsController < ApplicationController
+    before_action :set_blog, only: [:show, :edit, :update, :destroy]
+    before_action :require_logged_in!, only: [:new, :show, :edit, :update, :destroy]
+    before_action :edit_blog, only: [:edit, :destroy]
+  
+  
+  def index
+    @blogs = Blog.all
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = current_user.blogs.build(blog_params)
+    if @blog.save
+      BlogMailer.blog_mail(@blog).deliver
+      redirect_to blogs_path, notice: "ブログを作成しました！"
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @favorite = current_user.favorites.find_by(blog_id: @blog.id)
+  end
+
+  def edit
+  end
+
+  def update
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice:"ブログを削除しました！"
+  end
+
+  def confirm
+    @blog = current_user.blogs.build(blog_params)
+    render :new if @blog.invalid?
+  end
+  
+  def new
+    if params[:back]
+      @blog = Blog.new(blog_params)
+    else
+      @blog = Blog.new
+    end
+  end
+
+  def edit_blog
+    unless @blog.user_id == current_user.id  
+      redirect_to blogs_path, notice: "投稿者以外はブログを編集できません！！"
+    end
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content, :image, :image_cache)
+  end
+  
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
 end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,19 @@
 class FavoritesController < ApplicationController
+    
+    def create
+        favorite = current_user.favorites.create(blog_id: params[:blog_id])
+        redirect_to blogs_url, notice: "#{favorite.blog.user.name}さんのブログをお気に入り登録しました"
+      end
+  
+      def destroy
+        favorite = current_user.favorites.find_by(id: params[:id]).destroy
+        redirect_to blogs_url, notice: "#{favorite.blog.user.name}さんのブログをお気に入り解除しました"
+      end
+    end
+    
+  private
+    def fevorites_params
+      params.require(:fevorite).permit(:image, :image_cache)
+    end
+
 end
