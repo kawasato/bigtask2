3423d0db9b9240a2b8c45610b380725af20fa52b
USER <user@USER-no-MacBook-Air-2.local>
2018-12-17 22:57:45 +0900

    herokuエラーの修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,14 +3,13 @@ class FavoritesController < ApplicationController
     def create
         favorite = current_user.favorites.create(blog_id: params[:blog_id])
         redirect_to blogs_url, notice: "#{favorite.blog.user.name}さんのブログをお気に入り登録しました"
-      end
+    end
   
-      def destroy
+    def destroy
         favorite = current_user.favorites.find_by(id: params[:id]).destroy
         redirect_to blogs_url, notice: "#{favorite.blog.user.name}さんのブログをお気に入り解除しました"
-      end
     end
-    
+
   private
     def fevorites_params
       params.require(:fevorite).permit(:image, :image_cache)
