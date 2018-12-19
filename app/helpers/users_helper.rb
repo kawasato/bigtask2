f721f96352fca557abae639424e5fcc78268dfd2
USER <user@USER-no-MacBook-Air-2.local>
2018-12-19 22:11:46 +0900

    ログインしている本人以外プロフィールを閲覧、編集できないための実装

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,3 +1,9 @@
 module UsersHelper
-    
+    def choose_edit_or_new
+        if action_name == 'new' 
+          user_path
+        elsif action_name == 'edit'
+          user_path
+        end
+      end  
 end
