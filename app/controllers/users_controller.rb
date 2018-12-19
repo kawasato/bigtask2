17a34009017c7f89e9eed1b85a8a6f643bc3cec4
USER <user@USER-no-MacBook-Air-2.local>
2018-12-19 20:56:42 +0900

    ログインしている本人以外プロフィールを閲覧、編集できないための実装（途中２）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
-    before_action :edit_user, only: [:edit]
-    before_action :set_user, only: [:show, :edit, :update, :favorite]
-    before_action :require_logged_in!, only: [:show, :edit, :update, :favorite]
+  before_action :set_user, only: [:show, :edit, :update, :favorite]
+  before_action :edit_user, only: [ :edit]
+  before_action :require_logged_in!, only: [:show, :edit, :update, :favorite]
     
     def new
         @user = User.new
     end
 
-def create
+  def create
     @user = User.new(user_params)
     if @user.save
         redirect_to user_path(@user.id)
@@ -16,33 +16,29 @@ def create
   end
 end
 
-def show
+  def show
     @user = User.find(params[:id])
     
-end
-
-def edit
-end
-
-def update
-  if @user.update(user_params)
-    redirect_to user_path notice: "ユーザーを編集しました！"
-  else
-    render 'edit'
   end
-end
 
-
-
-def favorite
-  @user = User.find(params[:id])
-  @favorites_blogs = @user.favorites
-end
-
-def edit_user
-  unless @user.id == current_user.id  
-    redirect_to user_path, notice: "投稿者以外は編集できません！！"
+  def edit
   end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path notice: "ユーザーを編集しました！"
+    else
+      render 'edit'
+    end
+  end
+
+
+
+  def favorite
+    @user = User.find(params[:id])
+    @favorites_blogs = @user.favorites
+  end
+
 end
 
 
@@ -54,6 +50,10 @@ private
   end
   def set_user
     @user = User.find(params[:id])
+  end
+  def edit_user
+    unless @user.id == current_user.id  
+      redirect_to user_path, notice: "投稿者以外は編集できません！！"
   end
 end
 
